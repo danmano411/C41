f7ebb5e12c1f51221470948fe4cd32401bb58069
Dan Mano <danmano411@gmail.com>
2021-01-11 16:16:34 -0500

    made ranks for the players

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = 0;
   }
 
   getCount(){
@@ -32,4 +33,17 @@ class Player {
       allPlayers = data.val();
     })
   }
+
+  getRank(){
+    database.ref('rank').on("value", (data)=>{
+      this.rank = data.val()
+    })
+  }
+
+  static updateRank(rank){
+    database.ref('/').update({
+      rank:rank
+    })
+  }
+
 }
